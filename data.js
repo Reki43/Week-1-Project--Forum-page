c11a4c00746c03c5b62bc68ebfc734fc73c57e6a
Henry Wong <henrywong228@hotmail.com>
2024-05-02 01:17:40 -0700

    Creating Forum Page

---
+++ data.js
@@ -0,0 +1,38 @@
+var threads = [
+    {
+        title: "Thread 1",
+        author: "Aaron",
+        date: Date.now(),
+        conntent: "Thread content",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    },
+    {
+        title: "Thread 2",
+        author: "Aaron",
+        date: Date.now(),
+        conntent: "Thread content",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    }
+]
